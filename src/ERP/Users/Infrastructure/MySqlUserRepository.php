4309b9ea95d8502e0e55afee3918b673e04fdb83
Francisco Marcano <fjmn2001@gmail.com>
2021-01-23 08:51:16 -0400

    feat(): users changes v7. Ref sn

--- src/ERP/Users/Infrastructure/MySqlUserRepository.php
+++ src/ERP/Users/Infrastructure/MySqlUserRepository.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace Medine\ERP\Users\Infrastructure;
 
+use Illuminate\Support\Facades\DB;
 use Medine\ERP\Users\Domain\User;
+use Medine\ERP\Users\Domain\UserRepository;
 
-final class MySqlUserRepository implements \Medine\ERP\Users\Domain\UserRepository
+final class MySqlUserRepository implements UserRepository
 {
 
     public function save(User $user): void
     {
-        // TODO: Implement save() method.
+        DB::table('users')->insert([
+            'name' => $user->name(),
+            'email' => $user->email(),
+            'password' => $user->password()
+        ]);
     }
 }
