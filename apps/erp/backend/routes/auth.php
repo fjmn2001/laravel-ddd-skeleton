2b8aec77c4fb823ae2a529ad55f67a007272e270
Francisco Marcano <fjmn2001@gmail.com>
2021-01-22 23:12:28 -0400

    feat(): users changes v4. Ref sn

--- apps/erp/backend/routes/auth.php
+++ apps/erp/backend/routes/auth.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 use Medine\Apps\ERP\Backend\Controller\Auth\AuthController;
 
 Route::post('/auth/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->post('/auth/logout', [AuthController::class, 'logout']);
