efe2a6925cba703438091007eb1ed4a9ebfb31ca
Francisco Marcano <fjmn2001@gmail.com>
2021-01-29 06:13:31 -0400

    feat(): auth frontend v2. Ref sn

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -4,13 +4,18 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Http\JsonResponse;
 
 class Cors extends Middleware
 {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        if ($next($request) instanceof JsonResponse) {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        }
+
+        return $next($request);
     }
 }
--- apps/erp/frontend/src/modules/auth/Login.vue
+++ apps/erp/frontend/src/modules/auth/Login.vue
@@ -1,17 +1,43 @@
 <template>
-    <h1>Login</h1>
+    <div class="container">
+        <form @submit.prevent="submit">
+            <div>
+                <label>Username</label>
+                <input type="text" name="username" v-model="username">
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" name="username" v-model="password">
+            </div>
+            <div>
+                <button type="submit">Sing In</button>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {Component, Vue} from 'vue-property-decorator';
+import axios from "axios";
 
-@Component({
-    components: {
-        HelloWorld,
-    },
-})
-export default class Home extends Vue {}
+@Component
+export default class Login extends Vue {
+    username = '';
+    password = '';
+
+
+    submit() {
+        axios.post('http://localhost:8888/api/auth/login', {
+            username: this.username,
+            password: this.password
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+        }).then((response) => console.log(response.data));
+    }
+}
 </script>
 
 <style scoped>
