923e8e28e891e9a40b07a03b680ae4a9d224ae8c
Francisco Marcano <fjmn2001@gmail.com>
2021-01-29 22:17:01 -0400

    feat(): auth frontend v7. Ref sn

--- apps/erp/frontend/src/modules/auth/Login.vue
+++ apps/erp/frontend/src/modules/auth/Login.vue
@@ -1,26 +1,40 @@
 <template>
     <div class="container">
-        <form @submit.prevent="submit">
-            <div>
-                <label>Username</label>
-                <input type="text" name="username" v-model="username">
+        <div class=" center-login-med row ">
+            <div class="col-lg-7 col-md-9 col-sm-11 row-login text-center">
+                <img src="images/logo2_SF%201.svg" class="img-fluid mb-5">
+                <div class="div-text mb-5">
+                    <h4 class="font-italic font-weight-bold text-re"><b class="clr-text">Inicio de Sesión</b></h4>
+                </div>
+                <div class="input-group mb-5">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text "><img src="images/icons/@.svg"><br></span>
+                    </div>
+                    <input type="text" class="form-control " placeholder="Dirección de correo electrónico"
+                           aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-5">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text "><img src="images/icons/key.svg"></span>
+                    </div>
+                    <input type="text" class="form-control " aria-label="Username" aria-describedby="basic-addon1"
+                           placeholder="Contraseña">
+                </div>
+                <button type="button" class="btn btn-block btn-sm font-weight-bold linear-btn mb-3 text-white">
+                    Ingresar
+                </button>
+                <a class="btn btn-block  btn-sm mb-3 mt-4 btn-outline-info-med" href="register-company.html">Registrar
+                    Empresa</a>
+                <div class="div-text pt-3">
+                    <a class="olvidar-clave" href="recover-password.html">¿Olvidáste tu contraseña?</a>
+                </div>
+                <div class="pt-5">
+                    <p class="by mb-0">Diseñado y Desarrollado por &nbsp;<a class="olvidar-clave"
+                                                                            href="https://medine.dev/" target="_blank">MEDINE</a>
+                    </p>
+                </div>
             </div>
-            <div>
-                <label>Password</label>
-                <input type="password" name="username" v-model="password">
-            </div>
-            <div>
-                <button type="submit">Sing In</button>
-            </div>
-            <div v-if="this.$store.state.token">
-                <button type="button" @click="logout">Logout</button>
-            </div>
-            <pre>
-                token: {{ this.$store.state.token }}
-                <br>
-                isLogged: {{ this.$store.getters.isLogged }}
-            </pre>
-        </form>
+        </div>
     </div>
 </template>
 
