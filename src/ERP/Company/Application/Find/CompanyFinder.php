be7baefebd7a8a9a83731634ed63061b9171364e
gibmyx <gibmyxjobs@gmail.com>
2021-02-02 14:12:11 -0400

    a

--- src/ERP/Company/Application/Find/CompanyFinder.php
+++ src/ERP/Company/Application/Find/CompanyFinder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Medine\ERP\Company\Application\Find;
 
 
+use Medine\ERP\Company\Application\Response\CompanyResponse;
 use Medine\ERP\Company\Domain\ValueObjects\CompanyId;
 use Medine\ERP\Roles\Application\RolResponse;
 
@@ -17,15 +18,15 @@ final class CompanyFinder
         $this->finder = $finder;
     }
 
-    public function __invoke(CompanyFinderRequest $request): RolResponse
+    public function __invoke(CompanyFinderRequest $request): CompanyResponse
     {
         $rol = ($this->finder)(new CompanyId($request->id()));
 
-        return new RolResponse(
+        return new CompanyResponse(
             $rol->id()->value(),
             $rol->name()->value(),
-            $rol->description()->value(),
-            $rol->superuser()->value(),
+            $rol->status()->value(),
+            $rol->address()->value(),
             $rol->status()->value()
         );
     }
