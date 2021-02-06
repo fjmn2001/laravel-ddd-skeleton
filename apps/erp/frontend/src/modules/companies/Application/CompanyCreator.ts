b55f3f5683240a66bc855cb49fce1da50c362157
Francisco Marcano <fjmn2001@gmail.com>
2021-02-06 11:07:39 -0400

    feat(): adding prototype v9. Ref sn

--- apps/erp/frontend/src/modules/companies/Application/CompanyCreator.ts
+++ apps/erp/frontend/src/modules/companies/Application/CompanyCreator.ts
@@ -1,8 +1,22 @@
 import CompanyCreatorRequest from "@/modules/companies/Application/CompanyCreatorRequest";
+import HttpCompanyRepository from "@/modules/companies/Infrastructure/HttpCompanyRepository";
+import Company from "@/modules/companies/Domain/Company";
 
 export default class CompanyCreator {
+    repository: HttpCompanyRepository
+
+    constructor() {
+        this.repository = new HttpCompanyRepository();
+    }
+
     __invoke(request: CompanyCreatorRequest) {
-        console.log(request.name);
-        console.log(request.phone);
+        const $Company = new Company(
+            request.name,
+            request.state,
+            request.address,
+            request.phone
+        );
+        //
+        this.repository.save($Company);
     }
 }
