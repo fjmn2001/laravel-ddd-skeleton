96a07d8c19226ed89a2cefe4807cb15ccff0a0f1
pereira08 <jpyftsunami08@gmail.com>
2021-02-24 17:54:43 -0400

    '240221.3'

--- apps/erp/frontend/src/modules/companies/List.vue
+++ apps/erp/frontend/src/modules/companies/List.vue
@@ -85,16 +85,43 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="pb-4 pl-4 pr-4 pt-3" v-if="!hasData() && !loading()">
+                        <div class="pb-4 pl-4 pr-4 " v-if="!hasData() && !loading()">
                             <div class="no-resul">
                                 <i class="icon-doc color-blue1"></i>
                                 <h2>No se encontró ningún registro.</h2>
                             </div>
                         </div>
                         <div class="pb-4 pl-4 pr-4 pt-3" v-if="loading()">
-                            <div class="no-resul">
-                                <img src="@/assets/images/icons/log.gif" class="log-git">
-                                <h2>Cargando...</h2>
+                            <div class="pb-3 pl-4 pr-4 pre-loader pt-3">
+                                <div class="table-responsive">
+                                    <div class="l-hear">
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                    </div>
+                                    <div class="l-bod l-hear">
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                    </div>
+                                </div>
+                                <div class="justify-content-center mt-0 pt-0 row">
+                                    <div class="align-items-center col-md-6 d-flex justify-content-center offset-md-3 pb-3 pt-3 pagination">
+                                        <a class="btn btn-cicle"></a>
+                                        <a class="btn btn-cicle"></a>
+                                        <p class="p-pag">Página &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                                        <a class="btn btn-cicle"></a>
+                                        <a class="btn btn-cicle"></a>
+                                        <input type="text" class="inp-pag" style="margin-left: 7px;">
+                                    </div>
+                                    <div class="align-items-center col-md-3 d-flex justify-content-end pb-3 pt-3">
+                                        <p class="mr-4 p-pag">Mostrando 1 - 10 de 20</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
