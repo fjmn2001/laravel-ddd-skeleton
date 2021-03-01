fc949bf1ae8345e0d0540c98a44bc95d347378ab
Francisco Marcano <fjmn2001@gmail.com>
2021-03-01 17:51:19 -0400

    Adding loading template v1

--- apps/erp/frontend/src/modules/companies/components/GeneralsDetails.vue
+++ apps/erp/frontend/src/modules/companies/components/GeneralsDetails.vue
@@ -78,72 +78,7 @@
                 </div>
             </div>
         </div>
-        <div class="xcontainer">
-            <div class="align-items-center d-flex div-title-card justify-content-between row">
-                <div class="align-items-baseline d-sm-flex flex-md-row flex-sm-column">
-                    <h5 class="stitle-preloader xtitle-buscar"></h5>
-                    <p class="ml-md-3 ml-sm-0 pt-md-0 pt-sm-1 st-des-preloader xsubtitle-buscar"></p>
-                </div>
-                <div href="#des011" data-toggle="collapse" class="icon-preloader"></div>
-            </div>
-            <div id="des011" class="des01 m-3 pb-3">
-                <div class="mt-3 pl-3 pr-3 row">
-                    <div class="col-lg-3 col-md-6 col-sm-12 d-flex justify-content-center">
-                        <div class="ima-preloader"></div>
-                    </div>
-                    <div class="col-lg-9 col-md-6 col-sm-12 pt-4">
-                        <label class="label-preloader"></label>
-                        <div class="inp-preloader"></div>
-                        <div class="mt-3 row">
-                            <div class="col-lg-6 col-md-12">
-                                <label class="label-preloader"></label>
-                                <div class="inp-preloader"></div>
-                            </div>
-                            <div class="col-lg-6 col-md-12">
-                                <label class="label-preloader"></label>
-                                <div class="inp-preloader"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-3 pl-3 pr-3 row">
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <label class="label-preloader"></label>
-                        <div class="inp-preloader"></div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <label class="label-preloader"></label>
-                        <div class="inp-preloader"></div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <label class="label-preloader"></label>
-                        <div class="inp-preloader"></div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <label class="label-preloader"></label>
-                        <div class="inp-preloader"></div>
-                    </div>
-                </div>
-                <div class="mt-3 pl-3 pr-3 row">
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <label class="label-preloader"></label>
-                        <div class="inp-preloader"></div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <label class="label-preloader"></label>
-                        <div class="inp-preloader"></div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <label class="label-preloader"></label>
-                        <div class="inp-preloader"></div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <label class="label-preloader"></label>
-                        <div class="inp-preloader"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
     </div>
 </template>
 
