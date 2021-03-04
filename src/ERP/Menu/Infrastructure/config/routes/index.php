b9ec29c4b62b456fb7cf73345aca6b7a373f8e9f
Francisco Marcano <fjmn2001@gmail.com>
2021-03-03 22:14:52 -0400

    Topbar v3

--- src/ERP/Menu/Infrastructure/config/routes/index.php
+++ src/ERP/Menu/Infrastructure/config/routes/index.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use Medine\ERP\Menu\Infrastructure\Controller\LeftMenuGetController;
 use Medine\ERP\Menu\Infrastructure\Controller\TopMenuGetController;
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/menu/top_menu', TopMenuGetController::class);
+    Route::get('/menu/left_menu', LeftMenuGetController::class);
 });
