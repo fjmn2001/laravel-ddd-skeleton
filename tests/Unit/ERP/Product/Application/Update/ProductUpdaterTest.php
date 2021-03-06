e3e5311000c9eb45006150beccdfdac57bee313c
Daniel Marcano <dmarcanor2@gmail.com>
2021-03-06 12:30:47 -0400

    Feature(): mocking and applying object mother to product updater test

--- tests/Unit/ERP/Product/Application/Update/ProductUpdaterTest.php
+++ tests/Unit/ERP/Product/Application/Update/ProductUpdaterTest.php
@@ -10,28 +10,25 @@ use Medine\ERP\Product\Application\Create\CreateProductRequest;
 use Medine\ERP\Product\Application\Create\ProductCreator;
 use Medine\ERP\Product\Application\Update\ProductUpdater;
 use Medine\ERP\Product\Application\Update\UpdateProductRequest;
+use Medine\ERP\Product\Domain\Contracts\ProductRepository;
 use Medine\ERP\Product\Infrastructure\MySqlProductRepository;
 use Ramsey\Uuid\Uuid;
 use Tests\TestCase;
+use Tests\Unit\ERP\Product\Application\Create\CreateProductRequestMother;
+use Tests\Unit\ERP\Product\Domain\ProductMother;
 use Tests\Unit\ERP\Product\Infrastructure\InMemoryProductRepository;
 
 final class ProductUpdaterTest extends TestCase
 {
     use DatabaseTransactions;
 
-    protected $creator;
     protected $updater;
-    protected $faker;
 
     protected function setUp(): void
     {
-        $this->creator = new ProductCreator(
-            new MySqlProductRepository()
-        );
         $this->updater = new ProductUpdater(
-            new MySqlProductRepository()
+            new InMemoryProductRepository()
         );
-        $this->faker = Factory::create();
 
         parent::setUp();
     }
@@ -41,30 +38,30 @@ final class ProductUpdaterTest extends TestCase
      */
     public function it_should_update_an_existing_product_and_return_null()
     {
-        $UUID = Uuid::uuid4()->toString();
+        $request = CreateProductRequestMother::random();
+        $product = ProductMother::fromRequest($request);
 
-        ($this->creator)(new CreateProductRequest(
-            $UUID,
-            $this->faker->text(6),
-            $this->faker->name,
-            Uuid::uuid4()->toString(),
-            $this->faker->realText(255),
-            Uuid::uuid4()->toString()
-        ));
+        $repository = $this->createMock(ProductRepository::class);
+        $this->shouldFind($repository, $product);
+        $this->shouldUpdate($repository, $product);
 
-        $new_category_id = Uuid::uuid4()->toString();
-        $new_type_id = Uuid::uuid4()->toString();
-
-        $response = ($this->updater)(new UpdateProductRequest(
-            $UUID,
-            $this->faker->text(6),
-            'NAME TEST',
-            $new_category_id,
-            'TEST DESCRIPTION',
-            $new_type_id,
-            $this->faker->randomElement(['activo', 'inactivo'])
+        $response = (new ProductUpdater($repository))->__invoke(UpdateProductRequestMother::withId(
+            $product->id()->value()
         ));
 
         $this->assertNull($response);
+    }
+
+    private function shouldFind(\PHPUnit\Framework\MockObject\MockObject $repository, \Medine\ERP\Product\Domain\Entity\Product $product): void
+    {
+        $repository->method('find')
+            ->with($this->equalTo($product->id()))
+            ->willReturn($product);
+    }
+
+    private function shouldUpdate(\PHPUnit\Framework\MockObject\MockObject $repository, \Medine\ERP\Product\Domain\Entity\Product $product): void
+    {
+        $repository->method('update')
+            ->with($this->equalTo($product));
     }
 }
--- tests/Unit/ERP/Product/Domain/ProductCreatedAtMother.php
+++ tests/Unit/ERP/Product/Domain/ProductCreatedAtMother.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 
 namespace Tests\Unit\ERP\Product\Domain;
 
+use Medine\ERP\Product\Domain\ValueObjects\ProductCreatedAt;
+
 final class ProductCreatedAtMother
 {
+    public static function create(string $createdAt): ProductCreatedAt
+    {
+        return new ProductCreatedAt($createdAt);
+    }
 
+    public static function random(): ProductCreatedAt
+    {
+        return self::create(
+            SelectMother::fromValues(self::$status)
+        );
+    }
 }
