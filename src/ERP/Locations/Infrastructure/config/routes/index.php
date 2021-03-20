79ec9d4c9dbc142cc9310e017ae9acffb8bbc5f2
Francisco Marcano <fjmn2001@gmail.com>
2021-03-20 12:09:13 -0400

    Locations v4. Ref sn

--- src/ERP/Locations/Infrastructure/config/routes/index.php
+++ src/ERP/Locations/Infrastructure/config/routes/index.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Medine\ERP\Locations\Infrastructure\Controller\LocationGetController;
+use Medine\ERP\Locations\Infrastructure\Controller\LocationOptionsGetController;
 use Medine\ERP\Locations\Infrastructure\Controller\LocationPostController;
+use Medine\ERP\Locations\Infrastructure\Controller\LocationsBreadcrumbsGetController;
 
 Route::middleware('auth:api')->group(function () {
     //get
+    Route::get('/locations/options/{id}', LocationOptionsGetController::class);
     Route::get('/locations/{id}', LocationGetController::class);
 
     //post
     Route::post('/locations', LocationPostController::class);
+    Route::post('/locations/breadcrumbs', LocationsBreadcrumbsGetController::class);
 });
