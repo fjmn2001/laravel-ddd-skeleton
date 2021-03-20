dca6284594d3127b35de62ed4ea9396e458c1716
Francisco Marcano <fjmn2001@gmail.com>
2021-03-20 12:42:09 -0400

    Locations v5. Ref sn

--- src/ERP/Locations/Infrastructure/config/routes/index.php
+++ src/ERP/Locations/Infrastructure/config/routes/index.php
@@ -7,9 +7,11 @@ use Medine\ERP\Locations\Infrastructure\Controller\LocationGetController;
 use Medine\ERP\Locations\Infrastructure\Controller\LocationOptionsGetController;
 use Medine\ERP\Locations\Infrastructure\Controller\LocationPostController;
 use Medine\ERP\Locations\Infrastructure\Controller\LocationsBreadcrumbsGetController;
+use Medine\ERP\Locations\Infrastructure\Controller\LocationsCatalogsGetController;
 
 Route::middleware('auth:api')->group(function () {
     //get
+    Route::get('/locations/catalogs/{companyId}', LocationsCatalogsGetController::class);
     Route::get('/locations/options/{id}', LocationOptionsGetController::class);
     Route::get('/locations/{id}', LocationGetController::class);
 
