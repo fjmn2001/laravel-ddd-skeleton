1a6f65ac55b2da4202067499ec8e1eb7caf54842
Francisco Marcano <fjmn2001@gmail.com>
2021-06-12 21:33:17 -0400

    feat(): adding input at loctions forms v1

--- apps/erp/frontend/src/modules/locations/view/Create.vue
+++ apps/erp/frontend/src/modules/locations/view/Create.vue
@@ -3,6 +3,8 @@
         <div class="container-fluid main-conta">
             <div class="pl-1 pr-1">
                 <breadcrumbs :breadcrumbUrl="breadcrumbUrl"></breadcrumbs>
+
+                <generals-details></generals-details>
             </div>
         </div>
     </div>
@@ -11,10 +13,11 @@
 <script lang="ts">
 import {defineComponent} from "vue";
 import Breadcrumbs from '@/components/Breadcrums.vue';
+import GeneralsDetails from "@/modules/locations/component/GeneralsDetails.vue";
 import {useCore} from "@/modules/shared/use/useCore";
 
 export default defineComponent({
-    components: {Breadcrumbs},
+    components: {Breadcrumbs, GeneralsDetails},
     setup() {
         const {ERP_URL} = useCore();
         const breadcrumbUrl: string = ERP_URL + '/api/locations/breadcrumbs'
